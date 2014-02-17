e170e1edc5a798c6addc8b1ed059e24907784fc8
kuno_lab <kuno_lab@ananke.local>
2014-02-17 16:31:01 +0900

    Initial Commit

---
+++ BlockMatching/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  BlockMatching
+//
+//  Created by kuno_lab on 2014/02/17.
+//  Copyright (c) 2014年 kuno_lab. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
